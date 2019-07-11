9114133f2cf903582a84c96ab654d9bf20851e63
Benjamin L. Morrison <newben.hd@gmail.com>
2019-07-11 00:00:41 -0700

    [v0.2.0] location service api added to the app as module and its test cases were added

--- app.js
+++ app.js
@@ -3,27 +3,51 @@ const config = require("./config")("development");
 
 class app {
   constructor() {
-    this.apiUrl = `https://api.darksky.net/forecast/${
-      config.darkskySecretAPI
-    }/37.8267,-122.4233`;
+    this.apiUrl = `${config.darkskyUri}${config.darkskySecretAPI}`;
     this.currentTemperature = "";
     this.currentPrecipProbability = "";
   }
-  async printCurrent() {
-    let content = undefined;
+  async getDefaultForecast() {
     try {
-      const response = await rp({ uri: this.apiUrl, json: true });
+      const defaultUri = `${this.apiUrl}/42.3601,-71.0589`;
+      const response = await rp({ uri: defaultUri, json: true });
       this.currentTemperature = response.currently.temperature;
       this.currentPrecipProbability = response.currently.precipProbability;
-      content = `It is currently ${
-        response.currently.temperature
-      } degrees out. There is a ${
-        response.currently.precipProbability
-      }% chance of rain.`;
+      return response;
+      // content = `It is currently ${
+      //   response.currently.temperature
+      // } degrees out. There is a ${
+      //   response.currently.precipProbability
+      // }% chance of rain.`;
     } catch (error) {
       throw error;
     }
-    return content;
+  }
+  async getForecastByLatAndLong(longitude, latitude) {
+    try {
+      const uri = `${this.apiUrl}/${longitude},${latitude}`;
+      const response = await rp({ uri, json: true });
+      this.currentTemperature = response.currently.temperature;
+      this.currentPrecipProbability = response.currently.precipProbability;
+      return response;
+    } catch (error) {
+      throw error;
+    }
+  }
+  async getGeocode(query = {}) {
+    try {
+      const response = await rp(
+        `https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=${
+          config.mapboxAccessToken
+        }`,
+        {
+          json: true
+        }
+      );
+      return response.features[0].center;
+    } catch (error) {
+      throw error;
+    }
   }
 }
 
--- app.test.js
+++ app.test.js
@@ -2,27 +2,75 @@ const app = require("./app");
 const { expect, assert } = require("chai");
 
 describe("Weather app API test", () => {
-  let value = undefined;
+  let defaultForecast = undefined;
+  let geocode = undefined;
+  let forcastLatAndLong = undefined;
   before(async () => {
-    value = await app.printCurrent();
+    try {
+      defaultForecast = await app.getDefaultForecast();
+    } catch (error) {
+      throw error;
+    }
   });
-  describe("printCurrent()", () => {
-    it("Assert if printCurrent() returns string", () => {
-      assert.isString(value, "is not a string?");
+  before(async () => {
+    try {
+      geocode = await app.getGeocode();
+    } catch (error) {
+      throw error;
+    }
+  });
+
+  before(async () => {
+    try {
+      forcastLatAndLong = await app.getForecastByLatAndLong(
+        geocode[1],
+        geocode[0]
+      );
+    } catch (error) {
+      throw error;
+    }
+  });
+  describe("defaultForecast()", () => {
+    it("Assert if defaultForecast() returns an object", () => {
+      assert.isObject(defaultForecast, "is not an object?");
     });
-    it("Assert if printCurrent() includes current temperature", () => {
-      assert.include(
-        value,
-        app.currentTemperature,
-        "does not contain api data current temperature?"
+    it("Assert if defaultForecast() has property currently", () => {
+      assert.property(
+        defaultForecast,
+        "currently",
+        "does not contain property currently?"
       );
     });
-    it("Assert if printCurrent() includes current precipitation probability", () => {
-      assert.include(
-        value,
-        app.currentPrecipProbability,
-        "does not contain api data current precipitation probability?"
+    it("Assert if defaultForecast() returns an object that contains temperature property", () => {
+      assert.nestedProperty(
+        defaultForecast,
+        "currently.temperature",
+        "does not contain nested property currently.temperature?"
       );
+    });
+    it("Assert if defaultForecast() returns an object that contains precipitation probability property", () => {
+      assert.nestedProperty(
+        defaultForecast,
+        "currently.precipProbability",
+        "does not contain nested property currently.precipProbability?"
+      );
+    });
+  });
+  describe("getGeocode()", () => {
+    it("Assert if getGeocode() returns an array", () => {
+      assert.isArray(geocode, "is not an array?");
+    });
+    it("Assert if getGeocode() returns an array with length of two", () => {
+      assert.lengthOf(geocode, 2, "is not the array has two numbers?");
+    });
+    it("Assert if getGeocode() returns an array with two numbers", () => {
+      assert.isNumber(geocode[0], "is geocode[0] not a number?");
+      assert.isNumber(geocode[1], "is geocode[1] not a number?");
+    });
+  });
+  describe("getForcastByLatAndLong()", async () => {
+    it("Assert if getForcastByLatAndLong() returns an object", () => {
+      assert.isObject(forcastLatAndLong, "is not an object?");
     });
   });
 });
